f6dde51b3fb26047b4735d0105d854f3984a8ca9
mason.hows11 <mason.hows11@gmail.com>
2023-05-27 06:23:34 +0330

    add diePage

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -17,8 +17,7 @@ try {
     $pdo = new PDO("mysql:dbname=$database_config->db;host={$database_config->host}", $database_config->user, $database_config->pass);
     // set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
 } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    die();
+   diePage( "Connection failed: " . $e->getMessage());
+
 }
--- libraries/helpers.php
+++ libraries/helpers.php
@@ -1,1 +1,9 @@
 <?php
+
+function diePage($msg)
+{
+    echo "<div style='padding: 50px;margin: 0 auto;background: deepskyblue;width: 80%;border: 1px solid dodgerblue;border-radius:10px;font-family:sans-serif'>
+    <p style='text-align: center'>$msg</p>
+    </div>";
+    die();
+}
