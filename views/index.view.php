8511863d100899475e447e0d732852aa0416c970
mason.hows11 <mason.hows11@gmail.com>
2023-05-27 21:24:54 +0330

    rewrite task manager ui from scrach

--- views/index.view.php
+++ views/index.view.php
@@ -3,85 +3,87 @@
 <head>
     <meta charset="UTF-8">
     <title><?=  SITE_TITLE ?></title>
+    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/style.css">
 
 </head>
 <body>
 <!-- partial:index.partial.html -->
-<div class="page">
-    <div class="pageHeader">
-        <div class="title">Dashboard</div>
-        <div class="userPanel">
-            <a href=""><i class="fa fa-sign-out"></i></a>
-            <span class="username"></span>
-            <img src="/assets/img/person.jpg" width="40" height="40"/></div>
-    </div>
-    <div class="main">
-        <div class="nav">
-            <div class="searchbox">
-                <div><i class="fa fa-search"></i>
-                    <input type="search" placeholder="Search"/>
-                </div>
-            </div>
-            <div class="menu">
-                <div class="title">Folders</div>
-                <ul class="folder-list">
-                    <li class="">
-                        <a href=""><i class="fa fa-folder"></i>All</a>
-                    </li>
+<div class="container todolist-body mt-5 mb-5">
 
-                 <!--   <?php /*foreach ($folders as $folder): */?>
-                        <li class="<?/*=($_GET['folder_id'] == $folder->id) ? 'active' : ''*/?>">
-                            <a href="<?/*= site_url("?folder_id=$folder->id") */?>"><i class="fa fa-folder"></i><?/*=$folder->name*/?></a>
-                            <a href="?delete_folder=<?/*=$folder->id*/?>" class="remove"  onclick="return confirm('Are You Sure to delete this Item?\n<?/*=$folder->name*/?>');">x</a>
-                        </li>
-                    --><?php /*endforeach;*/?>
+    <div class="todolist-dashboard-header row">
 
-                </ul>
-            </div>
-            <div>
-                <input type="text" id="addFolderInput" style='width: 65%;margin-left:3%' placeholder="Add New Folder"/>
-                <button id="addFolderBtn" class="btn clickable">+</button>
-            </div>
+        <div class="my-3 col-lg-6">
+            <h4 class=" text-white">Dashboard</h4>
         </div>
-        <div class="view">
-            <div class="viewHeader">
-                <div class="title" style="width: 50%;">
-                    <input type="text" id="taskNameInput" style="width: 100%;margin-left:3%;line-height: 30px;" placeholder="Add New Task">
-                </div>
-                <div class="functions">
-                    <div class="button active">Add New Task</div>
-                    <div class="button">Completed</div>
-                </div>
-            </div>
-            <div class="content">
-                <div class="list">
-                    <div class="title">Today</div>
-                    <ul>
-                       <!-- <?php /*if (sizeof($tasks)): */?>
-                            <?php /*foreach ($tasks as $task): */?>
-                                <li class="<?/*=$task->is_done ? 'checked' : '';*/?>">
-                                    <i data-taskId="<?/*=$task->id*/?>" class="isDone clickable fa <?/*=$task->is_done ? 'fa-check-square-o' : 'fa-square-o';*/?> "></i>
-                                    <span><?/*=$task->title*/?></span>
-                                    <div class="info">
-                                        <span class='created-at'>Created At <?/*=$task->created_at*/?></span>
-                                        <a href="?delete_task=<?/*=$task->id*/?>" class="remove" onclick="return confirm('Are You Sure to delete this Item?\n<?/*=$task->title*/?>');">x</a>
-                                    </div>
-                                </li>
-                            <?php /*endforeach;*/?>
-                        <?php /*else: */?>
-                            <li>No Task Here ..</li>
-                        --><?php /*endif;*/?>
 
-                    </ul>
+        <div class="my-2 col-lg-6 d-flex justify-content-end">
+            <img src="<?= BASE_URL ?>/assets/img/person.jpg" class="rounded  user-image" alt>
+        </div>
+
+        <div>
+
+            <div class="todolist-body row">
+
+                <div class="left-sidebar col-lg-2 px-2">
+
+                    <div class="mb-3 mt-3">
+                        <input type="text" class="form-control" id="searchInput" placeholder="search">
+                    </div>
+
+                    <div class="mb-3 mt-3 px-2">
+                        FOLDERS
+                    </div>
+
+                    <div class="mb-3 mt-3 px-2">
+                        ALL
+                    </div>
+
+                    <div class="mb-3 mt-3">
+                        <input type="text" class="form-control" id="addFolderInput" placeholder="Add New Folder">
+                    </div>
+
+                    <div class="mb-3 mt-3">
+                        <button type="button" class="btn btn-success btn-sm">Add Folder</button>
+                    </div>
+
+                </div>
+
+                <div class="right-main col-lg-10 bg-white">
+
+                    <div class="main-header  border-bottom p-4 d-flex justify-content-between">
+
+                        <div class="main-title">
+                            <h3 class=" text-secondary">Manage Tasks</h3>
+                        </div>
+
+                        <div class="main-header-op d-flex flex-row ">
+
+                            <div>
+                                <button id="trash" class="btn btn-danger btn-sm mx-2">Trash</button>
+                            </div>
+                            <div>
+                                <button id="completed" class="btn btn-secondary btn-sm mx-2">Completed</button>
+                            </div>
+                            <div>
+                                <button id="addNewTask" class="btn btn-info btn-sm mx-2">Add New Task</button>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+
                 </div>
             </div>
+
         </div>
     </div>
 </div>
 <!-- partial -->
-<script src='/assets/js/jquery-3.5.1.min.js'></script>
-<script  src="/assets/js/script.js"></script>
+<script src='<?= BASE_URL ?>/assets/js/jquery-3.5.1.min.js'></script>
+<script src='<?= BASE_URL ?>/assets/js/bootstrap.bundle.min.js'></script>
+<script  src="<?= BASE_URL ?>/assets/js/script.js"></script>
 <script>
     $(document).ready(function(){
 
