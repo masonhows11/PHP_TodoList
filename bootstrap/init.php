29a3a6290c2818e11b40833a3916cbaf9de2cbd8
mason.hows11 <mason.hows11@gmail.com>
2023-05-27 06:26:28 +0330

    init refactor

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -2,22 +2,20 @@
 
 include "constants.php";
 include "config.php";
-include "libraries/helpers.php";
-include "libraries/lib-auth.php";
-include "libraries/lib-tasks.php";
-// for use all packages in vendor we should include
+// for use all packages in vendor composer system we should include
 // autoload.php file in init.php file
 include "vendor/autoload.php";
-
-
-
-
-
+// mysql pdo connection
 try {
     $pdo = new PDO("mysql:dbname=$database_config->db;host={$database_config->host}", $database_config->user, $database_config->pass);
     // set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
    diePage( "Connection failed: " . $e->getMessage());
-
 }
+
+
+include "libraries/helpers.php";
+include "libraries/lib-auth.php";
+include "libraries/lib-tasks.php";
+
