1f717f3d4b9b0f20adab5bbd41b35124de723831
mason.hows11 <mason.hows11@gmail.com>
2023-05-27 06:11:00 +0330

    add database  connection

--- bootstrap/config.php
+++ bootstrap/config.php
@@ -1,2 +1,8 @@
 <?php
 
+$database_config =(object)[
+    'host' => '127.0.0.1',
+    'user' => 'root',
+    'pass' => '1289..//',
+    'db' => 'todolistdb',
+];
--- bootstrap/init.php
+++ bootstrap/init.php
@@ -2,6 +2,23 @@
 
 include "constants.php";
 include "config.php";
+include "libraries/helpers.php";
+include "libraries/lib-auth.php";
+include "libraries/lib-tasks.php";
 // for use all packages in vendor we should include
 // autoload.php file in init.php file
 include "vendor/autoload.php";
+
+
+
+
+
+try {
+    $pdo = new PDO("mysql:dbname=$database_config->db;host={$database_config->host}", $database_config->user, $database_config->pass);
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    die();
+}
