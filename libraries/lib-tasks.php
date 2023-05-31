4f0706619257ccc9d1f3ee9897f7afba322226c7
mason.hows11 <mason.hows11@gmail.com>
2023-05-31 17:43:38 +0330

    change task status updated

--- libraries/lib-tasks.php
+++ libraries/lib-tasks.php
@@ -70,5 +70,14 @@ function deleteTask($task_id)
     return $stmt->rowCount();
 }
 
-
+function changeTaskStatus($taskId){
+    global $pdo;
+    $current_user_id = getCurrentUserId();
+    $sql = "select * from folders where user_id = $current_user_id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    // get all folder records as object type
+    $records = $stmt->fetchAll(PDO::FETCH_OBJ);
+    return $records;
+}
 
