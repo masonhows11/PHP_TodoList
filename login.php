947d3ccba776484f4b34d88972a8f91c431d8f0d
mason.hows11 <mason.hows11@gmail.com>
2023-05-31 19:10:51 +0330

    login & register views updated

--- login.php
+++ login.php
@@ -1,4 +1,13 @@
 <?php
 
 include "bootstrap/init.php";
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $action = $_GET['action'];
+    if($action = 'login'){
+       // login();
+    }
+}
+
 include "views/login.view.php";
--- register.php
+++ register.php
@@ -1,4 +1,13 @@
 <?php
 
 include "bootstrap/init.php";
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $action = $_GET['action'];
+    if ($action = 'register') {
+        register($_POST);
+    }
+}
+
 include "views/register.view.php";
