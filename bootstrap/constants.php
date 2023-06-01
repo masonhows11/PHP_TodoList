cca0052316f169a360702e9edf5c9b7b4eb08356
mason.hows11 <mason.hows11@gmail.com>
2023-06-01 19:20:18 +0330

    add session start

--- bootstrap/constants.php
+++ bootstrap/constants.php
@@ -1,6 +1,6 @@
 <?php
 
 const SITE_TITLE = 'Todo List Project';
-const BASE_URL = 'http://php_todolist.test';
+const BASE_URL = 'http://php_todolist.test/';
 const BASE_PATH = "C:/laragon/www/PHP_TodoLIst";
 // define('BASE_PATH',__DIR__);
